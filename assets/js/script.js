385c24dd14d300f81f80d7e799ad8c434442bab3
CarsdanDvorachek <cdvchek@gmail.com>
2021-10-06 13:22:16 -0400

    added meal functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,30 +12,49 @@ function pageDisplay(page) {
 }
 pageDisplay(1);
 
+// meal page handling - carsdan dvorachek
+function pageMealDisplay(page) {
+    let pageMealDisplay = document.querySelectorAll('.meal-page-' + page);
+    let other = Math.abs(page - 2) + 1;
+    let pageMealHide = document.querySelectorAll('.meal-page-' + other);
+    for (let i = 0; i < pageMealDisplay.length; i++) {
+        pageMealDisplay[i].setAttribute('style', 'display:inline;');
+    }
+    for (let i = 0; i < pageMealHide.length; i++) {
+        pageMealHide[i].setAttribute('style', 'display:none;');
+    }
+}
+pageMealDisplay(1);
+
 //declaring global variable for scoping purposes (used later)
 var drinksArr = [];
 var drinksArrInfo = [];
 var drinkNumber;
 var drinkShowing;
+var mealNumber;
+var mealShowing;
+var mealsArr = [];
+var mealsArrInfo = [];
 //page handling finished - carsdan dvorachek
-
 
 //search list
 // get api function - marco 
 var searchBtn = document.getElementById('searchBtn');
+var searchMealBtn = document.getElementById('search-meal-btn');
 var modIngredientList = document.getElementById('mod-ingr-list');
 var cocktailModHeader = document.getElementById('mod-name');
 var cocktailModImage = document.getElementById('mod-image');
 var cocktailModInstructions = document.getElementById('mod-instructions');
 var seeRecipeBtn = document.getElementById('seeRecipeBtn');
-var main = document.getElementById('main')
+var main = document.getElementById('main');
 
 function getApi() {
-    var ingredientInput = document.querySelector('.ingredients'); //first element of ingredients list
+    var ingredientInput = $('input[name="cocktail-input"]').val();
     // defining the search box variables
     console.log(ingredientInput);
-    var searchedIngredient = ingredientInput.innerText;
+    var searchedIngredient = ingredientInput;
     var searchedUrl = "https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=" + searchedIngredient;
+    console.log(searchedUrl);
     // fetching the corresponding search url 
     fetch(searchedUrl)
         .then(function (response) {
@@ -49,7 +68,6 @@ function getApi() {
             for (let i = 0; i < data.drinks.length; i++) {
                 var idUrl = "https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i=" + data.drinks[i].idDrink;
                 console.log(idUrl);
-                
                 
                 fetch(idUrl)
                 .then(function (response) {
@@ -151,6 +169,7 @@ function getApi() {
                             listMarkerEl.textContent = drinkShowing + 1;
                             console.log('display next recipe');
                         }
+                        textFit(cocktailNameEl);
                     }
         
                     function displayPrevRecipe() {
@@ -162,6 +181,7 @@ function getApi() {
                             listMarkerEl.textContent = drinkShowing + 1;
                             console.log('display prev recipe');
                         }
+                        textFit(cocktailNameEl);
                     }
         
                     function displayRecipe(index) {
@@ -177,95 +197,177 @@ function getApi() {
                         prevButtonEl.on('click', displayPrevRecipe);
                         nextButtonEl.on('click', displayNextRecipe);
                     }
-                    
-        
+
                     displayFirstRecipe();
+                    textFit(cocktailNameEl);
                     //prev and next button functions finished - carsdan dvorachek
-
-                        /*
-
-                        // display cocktail info onto the cocktail Mod
-                        console.log(data);
-                        // create new modal element
-                        var newMod = document.createElement("div");
-                        newMod.classList.add("modal");
-                        newMod.id = "modal" + (i + 1);
-                        main.appendChild(newMod);
-                        var newModContent = document.createElement("div");
-                        newModContent.classList.add("modal-content");
-                        newMod.appendChild(newModContent);
-                        // display corresponding drink name 
-                        let drinkName = data.drinks[0].strDrink;
-                        cocktailModHeader = drinkName;
-                        // create modal element
-                        modTitle = document.createElement("h4");
-                        modTitle.textContent = cocktailModHeader;
-                        newModContent.appendChild(modTitle);
-                        // see recipe button 
-                        
-                        // document.addEventListener('DOMContentLoaded', function() {
-                        //     var elems = document.querySelectorAll('.modal');
-                        //     var instances = M.Modal.init(elems, options);
-                        // });
-
-                        console.log(cocktailModHeader);
-                        // display corresponding cocktail image onto cocktail mod
-                        let modImageSource = data.drinks[0].strDrinkThumb;
-                        cocktailModImage.src = modImageSource;
-                        console.log(cocktailModImage.src);
-                        // create modal element
-                        modImageDisplay = document.createElement("img");
-                        modImageDisplay.src = cocktailModImage.src;
-                        newModContent.appendChild(modImageDisplay);
-
-                        // display instructions to mod
-                        let modInstructions = data.drinks[0].strInstructions;
-                        cocktailModInstructions = modInstructions;
-                        console.log(cocktailModInstructions);
-                        // create modal element
-                        modInstructionsDisplay = document.createElement("p");
-                        modInstructionsDisplay.textContent = cocktailModInstructions;
-                        newModContent.appendChild(modInstructionsDisplay);
-                        // create ul for modal
-                        modIngredientsDisplayList = document.createElement("ul");
-                        newModContent.appendChild(modIngredientsDisplayList);
-                        // display ingredients and amounts to mod
-                        for (let i = 0; i < 15; i++) {
-                            let listItem = data.drinks[0]["strIngredient" + (i + 1)];
-                            let listItemAmount = data.drinks[0]["strMeasure" + (i + 1)];
-                            // fix empty strings displaying 
-                            if ((listItem !== null)) {
-                                modIngredientItem = document.createElement("li");
-                                modIngredientItem.textContent = listItem;
-                                modIngredientsDisplayList.appendChild(modIngredientItem);
-                                // var node = document.createElement('li');
-                                // node.appendChild(document.createTextNode(listItem));
-                                // modIngredientList.appendChild(node);
-                                // console.log(node)
-                            }
-                            if (listItemAmount !== null) {
-                                modAmountItem = document.createElement("li");
-                                modAmountItem.textContent = listItemAmount;
-                                modIngredientsDisplayList.appendChild(modAmountItem);
-                                // var node = document.createElement('li');
-                                // node.appendChild(document.createTextNode(listItemAmount));
-                                // modIngredientList.appendChild(node);
-                                // console.log(node)
-                            }
-                        }
-                        */
                     });
-                    
             }
-            
-            
             // get api function - marco  
-
         });
     pageDisplay(2);
 }
 
 searchBtn.addEventListener('click', getApi);
+// get api function - marco  
+
+function getMealApi() {
+    var ingredientMealInput = $('input[name="meal-input"]').val();
+    // defining the search box variables
+    console.log(ingredientMealInput);
+    var searchedMealIngredient = ingredientMealInput;
+    var searchedMealUrl = "https://www.themealdb.com/api/json/v1/1/filter.php?i=" + searchedMealIngredient;
+    // fetching the corresponding search url 
+    fetch(searchedMealUrl)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.log(data);
+            mealNumber = data.meals.length;
+            
+            // searching returned cocktails by ID 
+            for (let i = 0; i < mealNumber; i++) {
+                var idMealUrl = "https://www.themealdb.com/api/json/v1/1/lookup.php?i=" + data.meals[i].idMeal;
+                console.log(idMealUrl);
+                
+                fetch(idMealUrl)
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (data) {
+                    console.log(data);
+                    //make empty array full of drink info
+                    let mealInfoObj =
+                    {
+                        mealName: "",
+                        mealImage: "",
+                        mealInstructions: "",
+                        mealIngredients: [],
+                        mealIngrAmt: []
+                    }
+                    mealInfoObj.mealName = data.meals[0].strMeal;
+                    mealInfoObj.mealImage = data.meals[0].strMealThumb;
+                    mealInfoObj.mealInstructions = data.meals[0].strInstructions;
+                    for (let i = 0; i < 15; i++) {
+                        let mealListItem = data.meals[0]["strIngredient" + (i + 1)];
+                        let mealListItemAmount = data.meals[0]["strMeasure" + (i + 1)];
+                        // fix empty strings displaying 
+                        if ((mealListItem !== null) && (mealListItem != "")) {
+                            mealInfoObj.mealIngredients.push(mealListItem);
+                            mealInfoObj.mealIngrAmt.push(mealListItemAmount);
+                        }
+                    }
+                    mealsArrInfo.push(mealInfoObj);
+                    console.log(mealsArrInfo);
+                    
+                    //grab modal items
+                    var modMealNameEl = document.querySelector('#mod-meal-name');
+                    var modMealImageEl = document.querySelector('#mod-meal-image');
+                    var modMealInstructionsEl = document.querySelector('#mod-meal-instructions');
+                    
+                    //function that updates modal
+                    function updateMealModal(){
+                        modMealNameEl.textContent = mealsArrInfo[mealShowing].mealName;
+                        modMealImageEl.setAttribute('src',mealsArrInfo[mealShowing].mealImage);
+                        modMealInstructionsEl.textContent = mealsArrInfo[mealShowing].mealInstructions;
+                        for (let i = 0; i < 15; i++) {
+                            var ingredientMealEl = document.querySelector('#meal-ingr'+(i+1));
+                            ingredientMealEl.setAttribute('style','display:none;');
+                            numOfMealIngr = mealsArrInfo[mealShowing].mealIngredients.length;
+                            if(i < numOfMealIngr){
+                                ingredientMealEl.textContent = '--' + mealsArrInfo[mealShowing].mealIngredients[i] + ' --' + mealsArrInfo[mealShowing].mealIngrAmt[i];
+                                console.log(mealInfoObj.mealIngredients[i]);
+                                console.log(mealInfoObj.mealIngrAmt[i]);
+                                ingredientMealEl.setAttribute('style','display:block;');
+                            }
+                        }
+                    }
+                    //on click event runs function
+                    var showMealRecipeBtn = $('#see-meal-recipe-btn');
+                    showMealRecipeBtn.on('click',updateMealModal)
+
+                    //prev and next button functions - carsdan dvorachek
+        
+                    let mealObj =
+                    {
+                        mealName: "",
+                        mealImage: ""
+                    }
+                    mealObj.mealName = mealInfoObj.mealName;
+                    mealObj.mealImage = mealInfoObj.mealImage;
+                    mealsArr.push(mealObj);
+                    console.log(mealsArr);
+        
+                    var mealNameEl = document.querySelector('#meal-name');
+                    var mealImageEl = document.querySelector('#meal-image');
+                    var mealListMarkerEl = document.querySelector('#meal-list-marker');
+                    var mealListMarkerLengthEl = document.querySelector('#meal-list-marker-length');
+        
+                    mealListMarkerLengthEl.textContent = mealNumber;
+        
+                    function displayFirstMealRecipe() {
+                        mealNameEl.textContent = mealsArr[0].mealName;
+                        mealImageEl.setAttribute('src', mealsArr[0].mealImage);
+                        mealShowing = 0;
+                        mealListMarkerEl.textContent = mealShowing + 1;
+                        console.log("display first meal recipe");
+                    }
+        
+                    function displayLastMealRecipe() {
+                        mealNameEl.textContent = mealsArr[mealNumber - 1].mealName;
+                        mealImageEl.setAttribute('src', mealsArr[mealNumber - 1].mealImage);
+                        mealShowing = mealNumber - 1;
+                        mealListMarkerEl.textContent = mealShowing + 1;
+                        console.log('display last meal recipe');
+                    }
+        
+                    function displayNextMealRecipe() {
+                        if ((mealShowing + 1) >= mealNumber) {
+                            displayFirstMealRecipe();
+                        } else {
+                            mealShowing++;
+                            displayMealRecipe(mealShowing);
+                            mealListMarkerEl.textContent = mealShowing + 1;
+                            console.log('display next meal recipe');
+                        }
+                    }
+        
+                    function displayPrevMealRecipe() {
+                        if ((mealShowing - 1) <= -1) {
+                            displayLastMealRecipe();
+                        } else {
+                            mealShowing--;
+                            displayMealRecipe(mealShowing);
+                            mealListMarkerEl.textContent = mealShowing + 1;
+                            console.log('display prev meal recipe');
+                        }
+                    }
+        
+                    function displayMealRecipe(index) {
+                        mealNameEl.textContent = mealsArr[index].mealName;
+                        mealImageEl.setAttribute('src', mealsArr[index].mealImage);
+                        mealShowing = index;
+                        mealListMarkerEl.textContent = mealShowing + 1;
+                    }
+                    var prevMealButtonEl = $('.prev-meal-btn');
+                    var nextMealButtonEl = $('.next-meal-btn');
+                    
+                    if(i == 0){
+                        prevMealButtonEl.on('click', displayPrevMealRecipe);
+                        nextMealButtonEl.on('click', displayNextMealRecipe);
+                    }
+
+                    displayFirstMealRecipe();
+                    //prev and next button functions finished - carsdan dvorachek
+                    });
+            }
+            // get api function - marco  
+        });
+    pageMealDisplay(2);
+}
+
+searchMealBtn.addEventListener('click', getMealApi);
 // get api function - marco  
 
 //modal initialization - carsdan dvorachek
@@ -280,41 +382,33 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //modal initialization finished - carsdan dvorachek
 
-// Kelly
-var addBtnEl = $('#add-btn');
-var cocktailListEl = $('#cocktail-list');
-var cocktailArr = [];
+// // Kelly
+// var addBtnEl = $('#add-btn');
+// var cocktailListEl = $('#cocktail-list');
+// var cocktailArr = [];
 
-// create function to handle form submission
-function handleFormSubmit(event) {
-    event.preventDefault();
+// // create function to handle form submission
+// function handleFormSubmit(event) {
+//     event.preventDefault();
 
-    // select form element by its `name` attribute and get its value
-    var cocktailItem = $('input[name="cocktail-input"]').val();
+//     // select form element by its `name` attribute and get its value
+//     var cocktailItem = $('input[name="cocktail-input"]').val();
 
-    // if there's nothing in the form entered, don't print to the page
-    if (!cocktailItem || cocktailArr.includes(cocktailItem)) {
-        console.log("Error, can't add ingredient!");
-        return;
-    }
+//     // if there's nothing in the form entered, don't print to the page
+//     if (!cocktailItem || cocktailArr.includes(cocktailItem)) {
+//         console.log("Error, can't add ingredient!");
+//         return;
+//     }
 
-    cocktailArr.push(cocktailItem);
+//     cocktailArr.push(cocktailItem);
 
+//     // print to the page
+//     cocktailListEl.append('<li class="ingredients">' + cocktailItem + '</li>');
 
+//     // clear the form input element
+//     $('input[name="cocktail-input"]').val('');
+// }
 
-    // print to the page
-    cocktailListEl.append('<li class="ingredients">' + cocktailItem + '</li>');
-
-    // clear the form input element
-    $('input[name="cocktail-input"]').val('');
-}
-
-// Create a submit event listener on the form element
-addBtnEl.on("click", handleFormSubmit);
-// Kelly
-
-
-// random movie generator -marco
-// random movie generator -marco
-
-
+// // Create a submit event listener on the form element
+// addBtnEl.on("click", handleFormSubmit);
+// // Kelly
